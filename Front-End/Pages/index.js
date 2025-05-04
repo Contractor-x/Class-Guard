c9edfc44e2632d3c3a92dbcc0c58d08cbac8020a
Collins Dada <140630132+Contractor-x@users.noreply.github.com>
2025-05-03 20:05:23 -0400

    Delete Front-End/Pages/index.js

--- Front-End/Pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from './Login';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as StudentDashboard } from './StudentDashboard';
-export { default as TeacherDashboard } from './TeacherDashboard';
